a3f3178ba4a010d3d12d43b8dcb560f06a47c2de
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-13 11:04:25 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,116 @@
+/**
+ * MakeCode extension for ESP8266 Wifi modules
+ */
+//% color=#009b5b icon="\uf1eb" block="ESP8266 Generic"
+namespace ESP8266ThingSpeak {
+
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let last_upload_successful: boolean = false
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 5000) break
+        }
+        return result
+    }
+
+    /**
+    * Initialize ESP8266 module and connect it to Wifi router
+    */
+    //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
+        wifi_connected = false
+        thingspeak_connected = false
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+CWMODE=1") // set to STA mode
+        sendAT("AT+RST", 1000) // reset
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
+    }
+
+    /**
+    * Connect to any server and upload data. It would not upload anything if it failed to connect to Wifi.
+    */
+    //% block="Upload data to custom server|URL/IP = %ip|payload = %uri"
+    //% ip.defl=api.foo.com
+    //% uri.defl=/api/upload?key=value
+    export function connectThingSpeak(ip: string, uri: string) {
+        if (wifi_connected) {
+            thingspeak_connected = false
+            sendAT("AT+CIPSTART=\"TCP\",\"" + ip + "\",80", 0) // connect to website server
+            thingspeak_connected = waitResponse()
+            basic.pause(100)
+            if (thingspeak_connected) {
+                last_upload_successful = false
+                let str: string = "GET " + uri + " HTTP/1.1 \u000D\u000A\u000D\u000A"
+                sendAT("AT+CIPSEND=" + (str.length + 2))
+                sendAT(str, 0) // upload data
+                last_upload_successful = waitResponse()
+                basic.pause(100)
+            }
+        }
+    }
+
+    /**
+    * Wait between uploads
+    */
+    //% block="Wait %delay ms"
+    //% delay.min=0 delay.defl=5000
+    export function wait(delay: number) {
+        if (delay > 0) basic.pause(delay)
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to Wifi
+    */
+    //% block="Wifi connected ?"
+    export function isWifiConnected() {
+        return wifi_connected
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to ThingSpeak
+    */
+    //% block="ThingSpeak connected ?"
+    export function isThingSpeakConnected() {
+        return thingspeak_connected
+    }
+
+    /**
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
+    */
+    //% block="Last data upload successful ?"
+    export function isLastUploadSuccessful() {
+        return last_upload_successful
+    }
+
+}
